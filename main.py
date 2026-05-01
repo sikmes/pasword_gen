794e7c1d4c4cb2e95e6c7c41da99c2e1544459fb
sikmes <sworackipiotr@gmail.com>
2026-05-01 15:24:29 +0200

    main.py

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import string
 
 app = FastAPI()
 
+visit_count = 0
 
 @app.get("/password")
 def generate_password(length: int = 12):
+    global visit_count
+    visit_count += 1
     characters = string.ascii_letters + string.digits + "!@#$%"
     password = "".join(random.choice(characters) for _ in range(length))
-    return {"password": password, "length": length}
+    return {"password": password, "length": length, "total_visits": visit_count}
+
+@app.get("/stats")
+def get_stats():
+    return {"total_visits": visit_count}
